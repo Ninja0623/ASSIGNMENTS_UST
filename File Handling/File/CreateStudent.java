112076edd3824ff2a2d2eec79d9dadb9100ae194
Kushanu Pg <129100774+Ninja0623@users.noreply.github.com>
2026-01-13 17:11:59 +0530

    done

---
+++ File Handling/File/CreateStudent.java
@@ -0,0 +1,19 @@
+package File;
+
+import java.io.FileWriter;
+
+public class CreateStudent {
+    public static void main(String[] args) {
+		FileWriter fw;
+		try {
+			fw=new FileWriter("src/Student.csv");
+			fw.write("id,name,marks\n");
+			fw.write("101,Ravi,78\n");
+			fw.write("102,Anita,85\n");
+			fw.write("103,Praveen,90\n");
+			fw.close();
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
---
+++ File Handling/File/FindByName.java
@@ -0,0 +1,34 @@
+package File;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class FindByName {
+    public static void main(String[] args) {
+		BufferedReader br;
+		String key="praveen";
+		try {
+			br=new BufferedReader(new FileReader("src/Student.csv"));
+			String ln;
+			while((ln=br.readLine())!=null)
+			{
+				String dt[]=ln.split(",");
+				if(dt[2].equals("marks"))
+				{
+					for(String e:dt)
+						System.out.print(e+" ");
+					System.out.println();
+				}
+				if(dt[1].equalsIgnoreCase(key))
+				{
+					for(String e:dt)
+						System.out.print(e+" ");
+					System.out.println();
+				}
+			}
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+    
+}
